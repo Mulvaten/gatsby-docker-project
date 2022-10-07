da36c2b8575861510c34c242bb8ff9bfa56188a6
tylerstratton87@gmail.com <tyler_stratton12@mymail.eku.edu>
2022-10-06 21:16:22 -0400

    Changed DB name

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,7 +1,11 @@
  <?php
-$servername = 172.19.0.2;
-$username = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
+$servername = getenv("MYSQL_DATABASE");
+$username = getenv("MYSQL_USER");
+$password = getenv("MYSQL_PASSWORD");
+
+echo $password;
+echo $username;
+echo $password;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
